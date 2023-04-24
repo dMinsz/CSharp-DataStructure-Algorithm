248afa4b18813c40fc4221e6e42dcea1f7156e25
dMinsz <dsminzks@gmail.com>
2023-04-24 19:27:00 +0900

    docs: Add Description
    우선순위큐의 설명 추가 및 변경

--- 5.Heap_PriorityQueue/PriorityQueue.cs
+++ 5.Heap_PriorityQueue/PriorityQueue.cs
@@ -11,9 +11,9 @@ namespace DataStructure
 
     //heap
     //=> 보무노드가 무조건 자식노드보다 우선순위가 높은상태를 힙상태라한다.
-    // 힙 자료구조는 2개의 자식을 가질수있는 이진 트리 자료구조이다.
+    // 힙 자료구조는 2개의 자식을 가질수있는 이진 트리 자료구조로 만들 수 있다.
     // heap을이용하여 우선순위큐 만들어보자.
-
+    // 2진 트리는 최소힙, 최대힙이있지만 최소힙으로 만들예정
 
     /*
      * 2진트리를 배열로 만들때 가지는 특성
@@ -30,7 +30,7 @@ namespace DataStructure
      * 주의: 완전 이진트리가 아니여서 비워진 자리가있을 수 있다.
      * 
      * 트리를 노드기반으로 노드들이 자식노드를 가리켜도되지만
-     * C# 은 노드기반 데이터는 GC에 부담을 주기때문에 배열로 구현하는게 좋다.
+     * C# 은 노드기반 자료구조는 GC에 부담을 주기때문에 배열기반의 자료구조로 구현하는게 좋다.
      */
 
     #endregion
@@ -43,7 +43,8 @@ namespace DataStructure
         }
 
         private List<Node> nodes; // 리스트의 어댑터 패턴으로 사용하자.
-        private IComparer<TPriority> comparer;
+        private IComparer<TPriority> comparer; // comparer 를 이용하면 더 다양한 자료구조를 받을수있다.
+                                               // 단! 비교가능(IComparable) 일때
 
         //기본생성자
         public PriorityQueue()
@@ -52,7 +53,7 @@ namespace DataStructure
             this.comparer = Comparer<TPriority>.Default;
         }
 
-        //comparer 를 지정했을때 생성자
+        //comparer 를 지정했을 때 생성자
         public PriorityQueue(IComparer<TPriority> comparer)
         {
             this.nodes = new List<Node>();
@@ -64,7 +65,7 @@ namespace DataStructure
 
         #region Heap 구조 사용에 필요한 메서드 구현
 
-        // 자식 인덱스의 부모노드의 인덱스 가져오기
+        // 부모노드의 인덱스 가져오기
         private int GetParentIndex(int childIndex)
         {
             return (childIndex - 1) / 2;
@@ -85,7 +86,7 @@ namespace DataStructure
         // Heap 에 데이터 넣기 알고리즘
         // 일단 데이터를 List 에 마지막 인덱스위치에 넣고
         // 우선순위를 확인하여 트리 의 데이터를 바꿔준다.
-        // 트리를 반절씩만 탐색하기때문에 속도우위가 있다.
+        // 트리를 반절씩만 탐색하기때문에 속도우위가 있다. (O(Logn))
 
         //Heap 에 데이터 넣기
         private void PushHeap(Node newNode)
@@ -120,8 +121,7 @@ namespace DataStructure
         // 가장 낮은 우선순위 즉 마지막 노드를
         // 첫 노드로 바꾸고 우선순위에 맞춰서 다시 트리를 구성한다.
 
-
-        //Heap에서 데이터 빼기 // 우선순위가 가장낮은 노드를 삭제
+        //Heap에서 데이터 빼기 // 우선순위가 가장낮은 노드를 삭제 // 마지막 노드를 삭제하는것과 같다.
         private void PopHeap()
         {
             Node lastNode = nodes[nodes.Count - 1]; // 마지막노드
@@ -136,6 +136,8 @@ namespace DataStructure
 
                 if (rightChildIndex < nodes.Count) //자식이 둘다있는경우
                 {
+                    // 완전 2진트리에서 왼쪽 자식이 먼저생기기때문에 오른쪽 자식이있으면 자식이 둘다있는 경우이다.
+
                     int compareIndex //좌측,우측 자식노드중에 우선순위가 높은걸 찾는다.
                         = comparer.Compare(nodes[leftChildIndex].Priority, nodes[rightChildIndex].Priority) 
                         < 0 ? leftChildIndex : rightChildIndex;
@@ -172,9 +174,6 @@ namespace DataStructure
                     break;
                 }
             }
-
-
-
         }
         #endregion
 
@@ -187,7 +186,7 @@ namespace DataStructure
             PushHeap(newNode);
         }
 
-        //현재 첫노드를 준다.
+        //우선순위가 가장 높은 노드를 준다. //현재 첫노드를 준다.
         public TElement Peek()
         {
             if (nodes.Count == 0)
@@ -196,7 +195,7 @@ namespace DataStructure
             return nodes[0].Element;
         }
 
-        //첫노드가 있는지 확인하고 있으면 첫노드준다.
+        //우선순위가 가장 높은 노드를 준다.//첫노드가 있는지 확인하고 있으면 첫노드준다.
         public bool TryPeek(out TElement element, out TPriority priority)
         {
             if (nodes.Count == 0)
@@ -232,7 +231,7 @@ namespace DataStructure
                 return false;
             }
 
-            Node rootNode = nodes[0];
+            Node rootNode = nodes[0]; // dequeue와 같다.
             element = rootNode.Element;
             priority = rootNode.Priority;
             PopHeap();
@@ -240,9 +239,6 @@ namespace DataStructure
         }
 
         #endregion
-
-
-
 
     }
 }
