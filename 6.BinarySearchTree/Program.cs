4898ad0097f6bcd92f54dd6822ae88218a09d0e0
dMinsz <dsminzks@gmail.com>
2023-04-25 15:38:09 +0900

    feat: Made BinarySearchTree Seting

---
+++ 6.BinarySearchTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6.BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Binary Search Tree!");
+        }
+    }
+}
