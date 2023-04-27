8cab5bb3c89b548567fb05949c3c7e924051c126
dMinsz <dsminzks@gmail.com>
2023-04-27 20:34:54 +0900

    feat: Add BackJoon hanoi,NandM Problem Solving

---
+++ BackJoon-15651/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace BackJoon_15651
+{
+    //https://www.acmicpc.net/problem/15651
+    internal class Program
+    {
+        public static int N, M;
+        public static StringBuilder sb = new StringBuilder();
+        public static int[] arr;
+        static void Main(string[] args)
+        {
+            string[] Question = Console.ReadLine().Split();
+
+            N = int.Parse(Question[0]); // 1~N 까지 조합한다.
+            M = int.Parse(Question[1]); // 길이가 M개 인
+
+            arr = new int[M]; // 조합된 수열 담을 배열
+
+            dfs(0); // 무조건 0 ~ M 까지 탐색해야한다.
+            Console.WriteLine(sb);
+
+        }
+
+        public static void dfs(int depth) 
+        {
+            // 재귀 깊이가 M과 같아지면 탐색과정에서 담았던 배열을 출력
+            if (depth == M)
+            {
+                for (int i = 0; i < M; i++)
+                {
+                    sb.Append(arr[i] + " ");
+                }
+                sb.AppendLine();
+                return;
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                arr[depth] = i; // 본인 깊이에 구해진 수 를 넣어둔다.
+                dfs(depth + 1); // 더 깊이 들어간다. // 즉 숫자를 하나씩 올리는중
+            }
+        }
+    }
+}
---
+++ BackJoonTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace BackJoonTest_11729
+{   //https://www.acmicpc.net/problem/11729
+    internal class Program
+    {
+        public static StringBuilder sbResult = new StringBuilder();
+        static void Main(string[] args)
+        {
+            int diskCount = int.Parse(Console.ReadLine());
+            int minMove = (int)Math.Pow(2, diskCount) - 1;
+
+            Console.WriteLine(minMove);
+            hanoi(diskCount, 1, 2, 3);
+            Console.WriteLine(sbResult);
+        }
+        
+        public static void hanoi(int disk, int from , int by, int to) 
+        {
+            if (disk == 1)
+            {
+                sbResult.Append(from);
+                sbResult.Append(' ');
+                sbResult.Append(to);
+                sbResult.AppendLine();
+            }
+            else 
+            {
+                hanoi(disk-1, from, to, by);
+                sbResult.Append(from);
+                sbResult.Append(' ');
+                sbResult.Append(to);
+                sbResult.AppendLine();
+                hanoi(disk - 1, by, from, to);
+            }
+        }
+    }
+}
