8d65c74f275f9f54ff87941a4b5f9e55a44f6df2
dMinsz <dsminzks@gmail.com>
2023-05-01 19:45:03 +0900

    feat: Made IList Search
    순차적인 데이터의 탐색 구현

---
+++ 10.Searching/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+
+    /// 그래프
+    // 정점의 모음과 이 정점을 잇는 간선의 모음의 결합 (정점:Vertex,간선:Edge)
+    // 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가짐 (트리와의 차이점)
+    // 간선의 방향성에 따라 단방향 그래프, 양방향 그래프가 있음
+    // 간선의 가중치에 따라   연결 그래프, 가중치 그래프가 있음
+    internal class Graph
+    {
+    }
+}
---
+++ 10.Searching/Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    internal class Search
+    {
+        //순차탐색 모든원소를 모두 확인
+        public static int SiquentialSearch<T>(in IList<T> list,in T item) where T: IEquatable<T>
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (item.Equals(list[i]))
+                { 
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //모든 원소가 정렬되어있을때에는 이진탐색을 할수있게된다. (반씩 탐색)
+        //list 가 정렬되어 있다고 가정, item 가 같은 값을 가지고있는 인덱스값을 준다.
+        public static int BinarySearch<T>(in IList<T> list, in T item) where T : IComparable
+        {
+            int low = 0;
+            int high = list.Count-1;
+
+            while(low <= high)//엇갈리지않을 때 까지 반복
+            {
+                int mid = (low+high) >> 1; // 나누기 2
+                int compare = item.CompareTo(list[mid]);
+                   
+                if (compare < 0)
+                    low = mid + 1;
+                else if (compare > 0)
+                    high = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;//찾는 값이 없다.
+        }
+    }
+}
