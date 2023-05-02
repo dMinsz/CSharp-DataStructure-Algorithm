bf5cc795a2cabc214bde5644a39c389787338699
dMinsz <dsminzks@gmail.com>
2023-05-02 17:56:18 +0900

    feat: Made Dijkstar Shortest Path Alogorithm

---
+++ 11.ShortestPath/Program.cs
@@ -0,0 +1,54 @@
+﻿using ShortestPath;
+
+namespace _11.ShortestPath
+{
+    //최단거리 알고리즘
+    //다익스트라 알고리즘 테스트
+    internal class Program
+    {
+        const int INF = 99999;
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[9, 9]
+             {
+                {   0, INF,   1,   7, INF, INF, INF,   5, INF},
+                { INF,   0, INF, INF, INF,   4, INF, INF, INF},
+                { INF, INF,   0, INF, INF, INF, INF, INF, INF},
+                {   5, INF, INF,   0, INF, INF, INF, INF, INF},
+                { INF, INF,   9, INF,   0, INF, INF, INF,   2},
+                {   1, INF, INF, INF, INF,   0, INF,   6, INF},
+                { INF, INF, INF, INF, INF, INF,   0, INF, INF},
+                {   1, INF, INF, INF,   4, INF, INF,   0, INF},
+                { INF,   5, INF,   2, INF, INF, INF, INF,   0}
+             };
+
+            int[] distance;
+            int[] path;
+            Dijkstra.ShortestPath(in graph, 0, out distance, out path);
+            Console.WriteLine("<Dijkstra>");
+            PrintResult(distance, path);
+
+        }
+
+        private static void PrintResult(int[] distance, int[] path)
+        {
+            Console.Write("Vertex");
+            Console.Write("\t");
+            Console.Write("dist");
+            Console.Write("\t");
+            Console.WriteLine("path");
+
+            for (int i = 0; i < distance.Length; i++)
+            {
+                Console.Write(i);
+                Console.Write("\t");
+                if (distance[i] >= INF)
+                    Console.Write("INF");
+                else
+                    Console.Write("{0:D3}", distance[i]);
+                Console.Write("\t");
+                Console.WriteLine(path[i]);
+            }
+        }
+    }
+}
