adb9d247c9c3c937440b694675200bb19b55272c
dMinsz <dsminzks@gmail.com>
2023-05-04 17:02:26 +0900

    feat: Make a foundation for Text RPG

---
+++ ToyProject_TextRPG/Game.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TextRPG
+{
+    internal class Game
+    {
+        private bool running = true;
+
+        public void Run()
+        {
+            Init();
+
+            while (running)
+            {
+                Render();
+                Update();
+            }
+
+            Release();
+        }
+
+        private void Init()
+        {
+            Console.CursorVisible = false;
+        }
+
+        private void Render()
+        {
+            Console.Clear();
+        }
+
+        private void Update()
+        {
+        }
+
+        private void Release()
+        {
+
+        }
+
+        public void MainMenu()
+        {
+        }
+
+        public void Map()
+        {
+        }
+
+        public void Battle()
+        {
+           
+        }
+
+        public void Inventory()
+        {
+        }
+
+        public void GameStart()
+        {
+        }
+
+        public void GameOver(string text = "")
+        {
+            Console.Clear();
+            running = false;
+
+        }
+    }
+}
